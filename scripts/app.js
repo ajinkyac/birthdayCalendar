8f704ae8720557cf9b1e2dd3a61d7611b9223523
Ajinkya Chhatre <ajinkya.chhatre@synacor.com>
2016-08-28 21:32:00 +0530

    Entry point
    - Application entry point, serves as the main module.

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,12 @@
+/*
+ * The application module. The global namespace of the application is kept as BCAL to keep the JS global scope clean.
+ */
+
 var BCAL = BCAL || {};
 
 (function(){
 
-  // Application bootstrap
+  // Application bootstrap, ideally this should not be here
   BCAL.dataServer = {
     path: "data/birthdays.json"
   };
